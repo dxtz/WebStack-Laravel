3ddc3566fd36dd2f970dcaf618fe24284ad1ab70
dxtz <xztime@126.com>
2019-11-16 19:32:32 +0800

    resources

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,15 +51,17 @@
         $(document).ready(function() {
             $(document).on('click', '.has-sub', function(){
                 var _this = $(this)
-                if(!$(_this).hasClass('expanded')) {
+                if(!$(this).hasClass('expanded')) {
                 setTimeout(function(){
                         _this.find('ul').attr("style","")
                 }, 300);
+                
                 } else {
                     $('.has-sub ul').each(function(id,ele){
+                        var _that = $(this)
                         if(_this.find('ul')[0] != ele) {
                             setTimeout(function(){
-                                _this.attr("style","")
+                                _that.attr("style","")
                             }, 300);
                         }
                     })
@@ -81,12 +83,24 @@
                 public_vars.$mainMenu.add(public_vars.$sidebarProfile).toggleClass('mobile-is-visible');
                 ps_destroy();
                 $("html, body").animate({
-                    scrollTop: $(this).offset().top - 30
+                    scrollTop: $($(this).attr("href")).offset().top - 30
                 }, {
                     duration: 500,
                     easing: "swing"
                 });
             });
+            return false;
+        });
+        var href = "";
+        var pos = 0;
+        $("a.smooth").click(function(e) {
+            $("#main-menu li").each(function() {
+                $(this).removeClass("active");
+            });
+            $(this).parent("li").addClass("active");
+            e.preventDefault();
+            href = $(this).attr("href");
+            pos = $(href).position().top - 30;
         });
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
